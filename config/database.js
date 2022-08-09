bcbc9e139fe233286cd3824f02cbae1d2d4cb295
jeric realubit <jericrealubit@gmail.com>
2022-08-10 11:29:18 +1200

    code improvement

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://jeric123:jeric123@cluster0.fkrqcuy.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+client.connect((err) => {
     if (err) throw err;
 });
 const collection = client.db("testdb").collection("testcollection");
